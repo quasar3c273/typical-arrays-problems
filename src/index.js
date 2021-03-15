3bc93f4b55df934f19dd4f7822adf3423259f8f0
Mikhail <48333089+quasar3c273@users.noreply.github.com>
2021-03-15 09:52:34 +0300

    init: tipical problem tast
    init: tipical problem tast

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array == undefined) return 0;
+  if (array.length == 0) return 0;
+  return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array == undefined) return 0;
+  if (array.length == 0) return 0;
+  let result = 0;
+  return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array == undefined) return 0;
+  if (array.length == 0) return 0;
+  let sumArr = array.reduce((prev, current) => prev + current, 0);
+  return sumArr / array.length;
 }
